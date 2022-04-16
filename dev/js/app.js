300e9d7d98a5741ad5b911bb9a1a93a2a0e29cdd
alesso425 <98488664+alesso425@users.noreply.github.com>
2022-04-16 09:29:12 -0400

    Created main JS code

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,41 @@
 window.onload = () => {
-    // Add your code below
+    let tempNum = 0, counterNum = 1;
+    let length = window.prompt(`Enter a number: `, `0`);
+    let input = parseInt(input, 10);
+    let table = document.getElementById(`table`);
+    let table2 = document.getElementById(`table2`);
+
+    let matrix = (input) => {
+        let firArray = [];
+        let arrLength = Math.pow(length, 2);
+        table.innerHTML += (`<tr>`);
+        for (int index = 0; index < arrLength; index++) {
+            firArray.push(index + 1);
+            if (firArray[index] % input == 0) {
+                table.innerHTML += (`</tr><tr><td>${firArray[index]}</td>`);
+            } else {
+                table.innerHTML += (`<td>${firArray[index]}<\td>`);
+            }
+        }
+        table.innerHTML += (`</tr>`);
+
+        for (int index = 0; index < arrLength; index++) {
+            if (((counterNum*input) - index) != firArray[index]) {
+                swap(firArray[index], firArray[index + 1]);
+            }
+        }
+        for (int index = 0; index < arrLength; index++) {
+            if (firArray[index] % input == 0) {
+                table2.innerHTML += (`</tr><tr><td>${firArray[index]}</td>`);
+            } else {
+                table2.innerHTML += (`<td>${firArray[index]}<\td>`);
+            }
+        }
+    }
+
+    let swap = (element1, element2) => {
+        tempNum = element1;
+        element1 = element2;
+        element2 = tempNum;
+    }
 };
